e1c55b2835390c0903a84e1488f9244b828083f5
abdallayasser <81267638+abdallayasser@users.noreply.github.com>
2021-03-24 10:13:16 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,80 @@
+from __future__ import division, print_function
+# coding=utf-8
+import sys
+import os
+import glob
+import re
+import numpy as np
+# Keras
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from keras.models import load_model
+from keras.preprocessing import image
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+
+# Define a flask app
+app = Flask(__name__)
+
+MODEL_PATH = 'soror.h5'
+
+
+model = load_model(MODEL_PATH)
+
+print('Model loaded. Check http://127.0.0.1:5000/')
+
+
+def model_predict(img_path, model):
+    img = image.load_img(img_path, target_size=(224, 224))
+
+    # Preprocessing the image
+    x = image.img_to_array(img)
+    # x = np.true_divide(x, 255)
+    x = np.expand_dims(x, axis=0)
+
+
+    x = preprocess_input(x, mode='caffe')
+
+    preds = model.predict(x)
+    return preds
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        preds = model_predict(file_path, model)
+
+        pred_class = preds.argmax(axis=1)
+        xx = {0: "bulging_Eye, follow the link for more information' https://bit.ly/3cthDmZ '",
+             1: "Cataracts, follow the link for more information' https://bit.ly/3ckyLLC '" ,
+             2: "crossed_Eye, follow the link for more information' https://bit.ly/30G1Z2k ' ",
+             3: "Glaucoma, follow the link for more information' https://bit.ly/3lk2Xe3 ' ",
+             4:"Healthy_Eye " ,
+             5:"uveitis, follow the link for more information' https://bit.ly/3bJuMcM ' "
+             }
+        result = str(pred_class)
+        result = result + "   " + xx[pred_class[0]]
+        return result
+    return None
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
